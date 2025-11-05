5f6ca814abd170a1150f3ffa06c234454ecdd3b2
Sachyam28 <sachyamshakya@gmail.com>
2025-11-05 21:21:25 +0530

    axios

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -13,7 +13,7 @@ export default function RegisterPage() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://web-stories-cms-player-5b9q.vercel.app//api/auth/register", {
+      const res = await axios.post("https://web-stories-cms-player-mpph.vercel.app/api/auth/register", {
         name,
         email,
         password,
