dd1fd6ba1c7b6af83f013d3fe1ae2e1f28c5b028
Sachyam28 <sachyamshakya@gmail.com>
2025-11-06 13:17:18 +0530

    vite pckg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
-export default {
-  server: {
-    port: 5173
-  },
+export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: "dist"
+    outDir: 'dist'
   }
-};
+})
